ababa86323670521a457780637e80253e14e3224
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-05-05 17:54:16 +0300

    add StartUp.cs, Box.cs

---
+++ Box.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> data; //field
+        public int Count 
+        {
+            get 
+            {
+                return data.Count; 
+            }         
+        }
+
+        public Box()
+        {
+            data = new List<T>();
+           
+        }
+
+        public void Add(T element)
+        {
+            data.Add(element);
+           
+        }
+
+        public T Remove() 
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Cannot remove from empty collection");
+            }
+
+            T result = data.Last();
+            data.Remove(result);
+
+            return result;
+        }
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace BoxOfT
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //generic means General, common, not specific, not specific data type;
+            //C#  allows you to define a generic classes, interfaces, abstract calsses, fields, methods, 
+            //static methods, properties, , events, delgates, and operators using type parameter.
+            // Type parameter <T> is a place holder for a particualr type specified when creating an insatnce
+            // of the generic type; A generic type is decalred by specifying a type parameter in an angle brackets
+            //after the type name e.g TypeName<T> where T is type parameter
+            //Generic classes are defineded by using a type parameter in an angel brackets after the class name; 
+            //class Fish<T>
+            //making a class generic, lets you to reuse the code, type safety, and performance;
+            //Advantiges of  Generics/ 1.Reasusability; 2.TypeSafety;3.BetterPerformance/
+            //Generic type provide btter performance because they rduce the need of for boxing, unboxing, 
+            //and typecasting of variables or objects.
+        }
+    }
+}
